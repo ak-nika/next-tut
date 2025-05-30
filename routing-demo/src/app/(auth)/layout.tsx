6a658952ff55384c7b78ac581cc8683dac6a8f8a
ak-nika <akingbayiojo@gmail.com>
2025-05-30 15:23:35 +0100

    Active links and styling active links

--- routing-demo/src/app/(auth)/layout.tsx
+++ routing-demo/src/app/(auth)/layout.tsx
@@ -1,9 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  const navLinks = [
+    { href: "/login", name: "Login" },
+    { href: "/register", name: "Register" },
+    { href: "/forgotPassword", name: "Forgot Password" },
+  ];
+
   return (
     <>
       <h2>Auth Pages</h2>
+      <div style={{ display: "flex", gap: "10px", marginBottom: "20px" }}>
+        {navLinks.map((link) => {
+          const isActive =
+            pathname === link.href ||
+            (pathname.startsWith(link.href) && link.href !== "/");
+
+          return (
+            <Link
+              style={{
+                fontWeight: isActive ? "bold" : "normal",
+                color: isActive ? "blue" : "",
+              }}
+              href={link.href}
+              key={link.href}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+      </div>
       {children}
     </>
   );
