947bb73db0505c4a06381f79d4fa2e95d8bf92c7
ak-nika <akingbayiojo@gmail.com>
2025-05-30 15:15:26 +0100

    Link navigation

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main page of our routing demo application.</p>
+
+      <Link href="/blog">Blog Page</Link>
+      <Link href="/products">Products Page</Link>
     </div>
   );
 }
--- routing-demo/src/app/products/page.tsx
+++ routing-demo/src/app/products/page.tsx
@@ -1,12 +1,25 @@
+import Link from "next/link";
+
 const Products = () => {
+  const productId = 100; // Example product ID, can be dynamic
+
   return (
     <div>
       <h1>Product Lists</h1>
 
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li>
+          <Link href="/products/1">Product 1</Link>
+        </li>
+        <li>
+          <Link href="/products/2">Product 2</Link>
+        </li>
+        <li>
+          <Link href="/products/3">Product 3</Link>
+        </li>
+        <li>
+          <Link href={`/products/${productId}`}>Product {productId}</Link>
+        </li>
       </ul>
     </div>
   );
