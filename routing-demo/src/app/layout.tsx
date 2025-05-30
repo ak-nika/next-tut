fe4a1385dccd5b9890c1f3ab6b2a66a74b459554
ak-nika <akingbayiojo@gmail.com>
2025-05-30 14:41:28 +0100

    Layout

--- routing-demo/src/app/layout.tsx
+++ routing-demo/src/app/layout.tsx
@@ -10,7 +10,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header style={{ backgroundColor: "lightblue", padding: "1rem" }}>
+          <p>Header</p>
+        </header>
+
+        <main>{children}</main>
+
+        <footer style={{ backgroundColor: "ghostwhite", padding: "1rem" }}>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
   );
 }
