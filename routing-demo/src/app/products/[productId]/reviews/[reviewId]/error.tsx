5e19ba4cdb2ee2c79f9cb83527dd61237d7bc0ed
ak-nika <akingbayiojo@gmail.com>
2025-06-05 11:57:48 +0100

    error recovery

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,9 +1,22 @@
 "use client";
 
-const Error = ({ error }: { error: Error }) => {
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
+  const router = useRouter();
+
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
   return (
     <div>
       <h1>{error.message}</h1>
+      <button onClick={reload}>Try again</button>
     </div>
   );
 };
