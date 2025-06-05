1127ba53c73cf40477455070ec0ad893c8747e42
ak-nika <akingbayiojo@gmail.com>
2025-06-05 11:52:47 +0100

    basic error handling

---
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Error = ({ error }: { error: Error }) => {
+  return (
+    <div>
+      <h1>{error.message}</h1>
+    </div>
+  );
+};
+
+export default Error;
--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,16 +1,23 @@
-import { notFound, redirect } from "next/navigation";
+import { notFound } from "next/navigation";
+
+const getRandomNumber = (count: number): number => {
+  return Math.floor(Math.random() * count);
+};
 
 const ReviewDetails = async ({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) => {
+  const random = getRandomNumber(2);
+  if (random === 1) {
+    throw new Error("An error occurred while fetching review details.");
+  }
+
   const { productId, reviewId } = await params;
 
   if (parseInt(reviewId) > 1000) {
-    // notFound();
-
-    redirect("/products");
+    notFound();
   }
   return (
     <div>
