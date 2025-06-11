0b118dd126b5f10ac59013ad5a36bf2ed04760dd
ak-nika <akingbayiojo@gmail.com>
2025-06-11 11:29:24 +0100

    headers in route handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,12 @@
-export async function GET() {
+import { headers } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders.get("Authorization"));
+
+  const headersList = await headers();
+  console.log(headersList.get("Authorization"));
+
   return new Response("Profile Page");
 }
