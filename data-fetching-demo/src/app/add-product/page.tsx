a8288a57e7f3d70eecc37ac485a02d0f70e130d2
ak-nika <akingbayiojo@gmail.com>
2025-06-13 18:21:03 +0100

    useActionState

--- data-fetching-demo/src/app/add-product/page.tsx
+++ data-fetching-demo/src/app/add-product/page.tsx
@@ -1,14 +1,50 @@
 import { addProduct } from "@/prismaDB";
 import { redirect } from "next/navigation";
 import SubmitButton from "@/components/SubmitButton";
+import { useActionState } from "react";
+
+type Errors = {
+  title?: string;
+  price?: string;
+  description?: string;
+};
+
+type FormState = {
+  errors: Errors;
+};
 
 export default function AddProduct() {
+  const initialState: FormState = {
+    errors: {},
+  };
+
+  const [state, formAction, isPending] = useActionState(
+    createProduct,
+    initialState
+  );
+
   const createProduct = async (formdata: FormData) => {
     "use server";
 
     const title = formdata.get("title")?.toString() as string;
     const price = formdata.get("price")?.toString() as string;
     const description = formdata.get("description")?.toString() as string;
+
+    const errors: Errors = {};
+
+    if (!title) {
+      errors.title = "Title is required";
+    }
+    if (!price) {
+      errors.price = "Price is required";
+    }
+    if (!description) {
+      errors.description = "Description is required";
+    }
+
+    if (Object.keys(errors).length > 0) {
+      return { errors };
+    }
 
     await addProduct(title, parseInt(price), description);
     redirect("/products-db");
@@ -20,7 +56,7 @@ export default function AddProduct() {
 
       <form
         className="p-4 space-y-4 max-w-96 mx-auto bg-gray-200 text-gray-800"
-        action={createProduct}
+        action={formAction}
       >
         <label>
           <span className="font-semibold block mb-2">Product Name</span>
@@ -29,9 +65,11 @@ export default function AddProduct() {
             placeholder="Enter product name..."
             name="title"
             className="w-full p-2 border rounded outline-none"
-            required
           />
         </label>
+        {state.errors.title && (
+          <p className="text-red-500">{state.errors.title}</p>
+        )}
 
         <label>
           <span className="font-semibold block my-2">Product Price</span>
@@ -40,9 +78,11 @@ export default function AddProduct() {
             placeholder="Enter product price..."
             name="price"
             className="w-full p-2 border rounded outline-none"
-            required
           />
         </label>
+        {state.errors.price && (
+          <p className="text-red-500">{state.errors.price}</p>
+        )}
 
         <label>
           <span className="font-semibold block my-2">Product Description</span>
@@ -51,11 +91,20 @@ export default function AddProduct() {
             placeholder="Enter product description..."
             name="description"
             className="w-full p-2 border rounded outline-none"
-            required
           />
         </label>
+        {state.errors.description && (
+          <p className="text-red-500">{state.errors.description}</p>
+        )}
 
-        <SubmitButton>Add Product</SubmitButton>
+        {/* <SubmitButton>Add Product</SubmitButton> */}
+        <button
+          type="submit"
+          disabled={isPending}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block mt-4 mx-auto cursor-pointer transition-colors disabled:bg-gray-500 disabled:cursor-not-allowed"
+        >
+          {isPending ? "Submitting..." : "Add Product"}
+        </button>
       </form>
     </div>
   );
