1a3ff0a00e486a4a04e67e243913dfdca67058ee
ak-nika <akingbayiojo@gmail.com>
2025-06-14 15:41:13 +0100

    sign in and sign out

---
+++ authentication-demo/src/components/Navigation.tsx
@@ -0,0 +1,32 @@
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
+
+const Navigation = () => {
+  return (
+    <nav className="bg-[var(--background)] border-b border-[var(--foreground)]/10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16 items-center">
+          <div className="flex-shrink-0">
+            <h1 className="text-xl font-semibold text-[var(--foreground)]">
+              Next.js App
+            </h1>
+          </div>
+          <div className="flex items-center gap-4">
+            <SignInButton mode="modal">
+              <button className="text-[var(--background)] bg-[var(--foreground)] cursor-pointer rounded px-4 py-2 font-semibold outline-none">
+                Sign in
+              </button>
+            </SignInButton>
+
+            <SignOutButton>
+              <button className="text-[var(--background)] bg-[var(--foreground)] cursor-pointer rounded px-4 py-2 font-semibold outline-none">
+                Sign out
+              </button>
+            </SignOutButton>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
